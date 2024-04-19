660ade5412ed3ab1e85a72abbc050661d6cffe38
Juan Carlos <juank133@hotmail.es>
2024-04-19 12:49:28 +0200

    primera subida

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -9,7 +9,6 @@ const Router = () => (
       <Routes>
         <Route index element={<Home />} />
         <Route path="/starwars" element={<AppStarwars />} />
-        <Route path="/profile/:id" element={<Profile />} />
         {/* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}
         <Route path="*" element={<div>404</div>} />
       </Routes>
